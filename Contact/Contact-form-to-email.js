8e65ed8c4fad602f203c5f524cca1c6d637c1687
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2024-04-16 02:54:40 +0530

    Update Contact-form-to-email.js

--- Contact/Contact-form-to-email.js
+++ Contact/Contact-form-to-email.js
@@ -14,7 +14,7 @@ function emailSend() {
 	Email.send({
 		SecureToken: "f1fd93e4-78df-4474-8db5-80daf880c520",
 		To: "mail.me.akashdip2001@gmail.com",
-		From: email,
+		From: "mail.me.akashdip2001@gmail.com",
 		Subject: "mail for public App - google play console",
 		Body: messageBody
 	}).then(
